9022fbad427ebf45db869150ac11218c40b5b557
Colin Jokisch <colindj1120@gmail.com>
2024-01-07 13:33:42 -0500

    Fixed module-info.java to export the classes so they can be used

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,22 @@ module DatabaseUtility {
     requires com.zaxxer.hikari;
     requires java.sql;
     requires java.sql.rowset;
+
+    exports io.github.colindj1120.database;
+    opens io.github.colindj1120.database;
+
+    exports io.github.colindj1120.database.consumers;
+    opens io.github.colindj1120.database.consumers;
+
+    exports io.github.colindj1120.database.models;
+    opens io.github.colindj1120.database.models;
+
+    exports io.github.colindj1120.database.processors;
+    opens io.github.colindj1120.database.processors;
+
+    exports io.github.colindj1120.database.runnables;
+    opens io.github.colindj1120.database.runnables;
+
+    exports io.github.colindj1120.database.suppliers;
+    opens io.github.colindj1120.database.suppliers;
 }
